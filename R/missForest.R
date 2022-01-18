3d07a71c1c74757fc04e8515117890e62ea6e48b
Elena Albu <elena.albu@kuleuven.be>
2022-01-18 18:49:17 +0100

    more cleaning

--- R/missForest.R
+++ R/missForest.R
@@ -4,11 +4,8 @@
 #'
 #' @param xmis matrix / dataframe containing missing values
 #' @param maxiter maximum number of iterations
-#' @param ntree number of trees in the forest (default 500)
-#' @param variablewise logical; return OOB errors for each variable separately
 #' @param decreasing (boolean) if TRUE the columns are sorted with decreasing amount of missing values
 #' @param verbose (boolean) if TRUE then missForest returns error estimates, runtime and if available true error during iterations
-#' @param mtry how many variables should be tried randomly at each node
 #' @param class.weights list of priors of the classes in the categorical variables
 #' @param xtrue complete data matrix
 #'
@@ -17,13 +14,12 @@
 
 missForest <- function(xmis,
                        maxiter = 10,
-                       ntree = 500,
                        variablewise = FALSE,
                        decreasing = FALSE,
                        verbose = FALSE,
-                       mtry = floor(sqrt(ncol(xmis))),
                        class.weights = NULL,
-                       xtrue = NA) {
+                       xtrue = NA,
+                       ...){
 
   n <- nrow(xmis)
   p <- ncol(xmis)
@@ -80,57 +76,42 @@ missForest <- function(xmis,
   # extract missingness pattern
   NAloc <- is.na(xmis)            # where are missings
   noNAvar <- apply(NAloc, 2, sum) # how many are missing in the vars
-  sort.j <- order(noNAvar)        # indices of increasing amount of NA in vars
-  if (decreasing)
-    sort.j <- rev(sort.j)
-  sort.noNAvar <- noNAvar[sort.j]
+  # imputation sequence, first the lowest missingness column if decreasing = FALSE
+  impute_sequence <- names(sort(noNAvar, decreasing = decreasing))
 
-  ## output
-  Ximp <- vector('list', maxiter)
-
-  ## initialize parameters of interest
+  # initialize vectors to trace the errors for each column
   iter <- 0
-  k <- length(unique(varType))
-  convNew <- rep(0, k)
-  convOld <- rep(Inf, k)
-  OOBerror <- numeric(p)
-  names(OOBerror) <- varType
+  err_new <- rep(0, p)
+  names(err_new) <- col_names
+  err_old <- rep(Inf, p)
+  names(err_old) <- col_names
+  err_OOB <- numeric(p)
+  names(err_OOB) <- col_names
 
-  ## setup convergence variables w.r.t. variable types
-  if (k == 1){
-    if (unique(varType) == 'numeric'){
-      names(convNew) <- c('numeric')
-    } else {
-      names(convNew) <- c('factor')
-    }
-    convergence <- c()
-    OOBerr <- numeric(1)
-  } else {
-    names(convNew) <- c('numeric', 'factor')
-    convergence <- matrix(NA, ncol = 2)
-    OOBerr <- numeric(2)
-  }
+  # gets a factor variable, returns a matrix of binary variables
+  make_binary <- function(x) {
 
-  ## function to yield the stopping criterion in the following 'while' loop
-  stopCriterion <- function(varType, convNew, convOld, iter, maxiter){
-    k <- length(unique(varType))
-    if (k == 1){
-      (convNew < convOld) & (iter < maxiter)
-    } else {
-      #((convNew[1] < convOld[1]) | (convNew[2] < convOld[2])) & (iter < maxiter)
-      (convNew[1] + convNew[2] < convOld[1] + convOld[2])  & (iter < maxiter)
+    unique_vals <- levels(x)
+    x_binary <- matrix(ncol = length(unique_vals), nrow = length(x))
+
+    for (i in 1:length(unique_vals)) {
+      x_binary[,i] <- ifelse(x == unique_vals[[i]], 1, 0)
     }
+
+    return(x_binary)
+
   }
 
   models <- list()
+
   ## iterate missForest
-  while (stopCriterion(varType, convNew, convOld, iter, maxiter)){
+  while (sum(err_new) < sum(err_old) & iter < maxiter){
 
     models[[iter + 1]] <- list()
 
     if (iter != 0){
-      convOld <- convNew
-      OOBerrOld <- OOBerr
+      err_old <- err_new
+      OOBerrOld <- err_OOB
     }
 
     cat("  missForest iteration", iter+1, "in progress...")
@@ -138,43 +119,38 @@ missForest <- function(xmis,
     t.start <- proc.time()
     ximp.old <- ximp
 
-    for (s in 1 : p) {
-      varInd <- sort.j[s]
+    for (col in impute_sequence){
 
-      obsi <- !NAloc[, varInd]
-      misi <- NAloc[, varInd]
-      obsY <- ximp[obsi, varInd]
-      obsX <- ximp[obsi, seq(1, p)[-varInd]]
-      misX <- ximp[misi, seq(1, p)[-varInd]]
-      typeY <- varType[varInd]
-      if (typeY == "numeric") {
+      obsi <- !NAloc[, col]
+      misi <- NAloc[, col]
+      obsY <- ximp[obsi, col]
+      obsX <- ximp[obsi, names(ximp)!=col]
+      misX <- ximp[misi, names(ximp)!=col]
 
-        #RF <- randomForest( x = obsX,
-        #                    y = obsY,
-        #                    ntree = ntree,
-        #                    mtry = mtry,
-        #                    replace = replace,
-        #                    sampsize = if (!is.null(sampsize)) sampsize[[varInd]] else
-        #                      if (replace) nrow(obsX) else ceiling(0.632 * nrow(obsX)),
-        #                    nodesize = if (!is.null(nodesize)) nodesize[1] else 1,
-        #                    maxnodes = if (!is.null(maxnodes)) maxnodes else NULL)
-        #
+      if (varType[col] == "numeric") {
+
         RF <- ranger(x = obsX,
                      y = obsY,
-                     num.trees = ntree,
-                     mtry = mtry)
+                     ...)
 
-        ## record out-of-bag error (MSE)
-        OOBerror[varInd] <- RF$prediction.error
+        # record out-of-bag error (MSE)
+        err_OOB[col] <- RF$prediction.error
+        # NMRSE
+        err_OOB[col] <- sqrt(RF$prediction.error/var(as.vector(as.matrix(xmis[, col])),
+                                                         na.rm = TRUE))
+
+        #sqrt(mean((ximp[mis] - xtrue[mis])^{2}) / var(xtrue[mis]))
 
         # save model
-        models[[iter + 1]][[varInd]] <- RF
+        models[[iter + 1]][[col]] <- RF
 
         if (nrow(misX) > 0) { # if the column is not complete
           misY <- predict(RF, misX)$predictions
         } else {
           misY <- c()
         }
+        ximp[misi, col] <- misY
+        # TODO: can I already update the column here? why not?
 
       } else {
         obsY <- factor(obsY)
@@ -182,36 +158,21 @@ missForest <- function(xmis,
         if (length(summarY) == 1) {
           misY <- factor(rep(names(summarY), sum(misi)))
         } else {
-          #RF <- randomForest(x = obsX,
-          #                   y = obsY,
-          #                   ntree = ntree,
-          #                   mtry = mtry,
-          #                   replace = replace,
-          #                   classwt = if (!is.null(classwt)) classwt[[varInd]] else
-          #                     rep(1, nlevels(obsY)),
-          #                   cutoff = if (!is.null(cutoff)) cutoff[[varInd]] else
-          #                     rep(1 / nlevels(obsY), nlevels(obsY)),
-          #                   strata = if (!is.null(strata)) strata[[varInd]] else obsY,
-          #                   sampsize = if (!is.null(sampsize)) sampsize[[varInd]] else
-          #                     if (replace) nrow(obsX) else ceiling(0.632 * nrow(obsX)),
-          #                   nodesize = if (!is.null(nodesize)) nodesize[2] else 5,
-          #                   maxnodes = if (!is.null(maxnodes)) maxnodes else NULL)
 
           RF <- ranger(x = obsX,
                        y = obsY,
-                       num.trees = ntree,
-                       mtry = mtry,
-                       class.weights = if (!is.null(class.weights)) class.weights[[varInd]], # TODO: test this and cutoff and strata
-                       probability = TRUE)
+                       #class.weights = if (!is.null(class.weights)) class.weights[[varInd]], # TODO: test this and cutoff and strata
+                       probability = TRUE,
+                       ...)
 
           # record out-of-bag error (Brier score when probability = TRUE)
-          OOBerror[varInd] <- RF$prediction.error
+          err_OOB[col] <- RF$prediction.error
+
 
           # save model
-          models[[iter + 1]][[varInd]] <- RF
+          models[[iter + 1]][[col]] <- RF
 
-          ## predict missing parts of Y
-          #misY <- predict(RF, misX)
+          # predict missing parts of Y
           if (nrow(misX) > 0) { # if the column is not complete
             #misY <- predict(RF, misX)$predictions
 
@@ -223,89 +184,37 @@ missForest <- function(xmis,
           } else {
             misY <- c()
           }
+          ximp[misi, col] <- misY
         }
       }
-      ximp[misi, varInd] <- misY
+      #ximp[misi, col] <- misY
+
+      # save convergence
+      if (varType[col] == "numeric"){
+        err_new[col] <- sum((ximp[, col] - ximp.old[, col])^2) / sum(ximp[, col]^2)
+        # TODO: alternative: only check for misi?
+      } else {
+
+        ximp_old_binary <- make_binary(ximp.old[, col])
+        ximp_new_binary <- make_binary(ximp[, col])
+
+        mean_observed <- mean(ximp_old_binary) # old
+        bs_baseline <- mean((mean_observed - ximp_old_binary)^2)
+
+        err_new[col] <- mean((ximp_new_binary - ximp_old_binary)^2) / bs_baseline
+        # this should be 1 - BSS
+
+      }
 
     }
 
-    names(models[[iter + 1]]) <- col_names
+    #names(models[[iter + 1]]) <- col_names
 
     cat('done!\n')
 
     iter <- iter + 1
-    Ximp[[iter]] <- ximp
-
-    t.co2 <- 1
-    ## check the difference between iteration steps
-    for (t.type in names(convNew)){
-      t.ind <- which(varType == t.type)
-      if (t.type == 'numeric'){
-        convNew[t.co2] <- sum((ximp[, t.ind] - ximp.old[, t.ind])^2) / sum(ximp[, t.ind]^2)
-      } else {
-        #dist <- sum(as.character(as.matrix(ximp[, t.ind])) != as.character(as.matrix(ximp.old[, t.ind])))
-        #convNew[t.co2] <- dist / (n * sum(varType == 'factor'))
-
-        # TODO: return probabilities
-        # TODO: test for binary and categorical
-
-        # gets a factor variable, returns a matrix of binary variables
-        make_binary <- function(x) {
-
-          unique_vals <- levels(x)
-          x_binary <- matrix(ncol = length(unique_vals), nrow = length(x))
-
-          for (i in 1:length(unique_vals)) {
-            x_binary[,i] <- ifelse(x == unique_vals[[i]], 1, 0)
-          }
-
-          return(x_binary)
-
-        }
-
-        conv_new_cols <- c()
-
-        for (t in t.ind){
-
-          ximp_old_binary <- make_binary(ximp.old[, t])
-          ximp_new_binary <- make_binary(ximp[, t])
-
-          mean_observed <- mean(ximp_old_binary) # old
-          bs_baseline <- mean((mean_observed - ximp_old_binary)^2)
-
-          conv_new_cols <- c(conv_new_cols, mean((ximp_new_binary - ximp_old_binary)^2) / bs_baseline)
-          # this should be 1 - BSS
-        }
-
-        convNew[t.co2] <- mean(conv_new_cols)
-      }
-      t.co2 <- t.co2 + 1
-    }
 
     ## compute estimated imputation error
-    if (!variablewise){
-      NRMSE <- sqrt(mean(OOBerror[varType == 'numeric'])/
-                      var(as.vector(as.matrix(xmis[, varType == 'numeric'])),
-                          na.rm = TRUE))
-      PFC <- mean(OOBerror[varType == 'factor'])
-      if (k == 1){
-        if (unique(varType) == 'numeric'){
-          OOBerr <- NRMSE
-          names(OOBerr) <- 'NRMSE'
-        } else {
-          OOBerr <- PFC
-          names(OOBerr) <- 'PFC'
-        }
-      } else {
-        OOBerr <- c(NRMSE, PFC)
-        names(OOBerr) <- c('NRMSE', 'PFC')
-      }
-    } else {
-      OOBerr <- OOBerror
-      names(OOBerr)[varType == 'numeric'] <- 'MSE'
-      names(OOBerr)[varType == 'factor'] <- 'PFC'
-    }
-
     if (any(!is.na(xtrue))){
       err <- suppressWarnings(mixError(ximp, xmis, xtrue))
     }
@@ -316,32 +225,37 @@ missForest <- function(xmis,
       if (any(!is.na(xtrue))){
         cat("    error(s):", err, "\n")
       }
-      cat("    estimated error(s):", OOBerr, "\n")
-      cat("    difference(s):", convNew, "\n")
+      cat("    OOB error(s):", err_OOB, "\n")
+      cat("    OOB error(s) total:", sum(err_OOB), "\n")
+      cat("    new error(s):", err_new, "\n")
+      #cat("    difference(s):", err_new, "\n")
+      cat("    difference(s) total:", sum(err_old) - sum(err_new) , "\n")
+      cat("    difference(s):", err_old - err_new, "\n")
+
       cat("    time:", delta.start[3], "seconds\n\n")
     }
-  }#end while((convNew<convOld)&(iter<maxiter)){
+  }#end while((err_new<err_old)&(iter<maxiter)){
 
   ## produce output w.r.t. stopping rule
   if (iter == maxiter){
     if (any(is.na(xtrue))){
-      out <- list(ximp = Ximp[[iter]], OOBerror = OOBerr)
+      out <- list(ximp = ximp, OOBerror = err_OOB)
     } else {
-      out <- list(ximp = Ximp[[iter]], OOBerror = OOBerr, error = err)
+      out <- list(ximp = ximp, OOBerror = err_OOB, error = err)
     }
   } else {
     if (any(is.na(xtrue))){
-      out <- list(ximp = Ximp[[iter - 1]], OOBerror = OOBerrOld)
+      out <- list(ximp = ximp.old, OOBerror = OOBerrOld)
     } else {
-      out <- list(ximp = Ximp[[iter - 1]], OOBerror = OOBerrOld,
-                  error = suppressWarnings(mixError(Ximp[[iter - 1]], xmis, xtrue)))
+      out <- list(ximp = ximp.old, OOBerror = OOBerrOld,
+                  error = suppressWarnings(mixError(ximp.old, xmis, xtrue)))
     }
   }
 
   # save single initialization as list
   out$init <- var_single_init
   out$models <- models
-  out$impute_sequence <- col_names[sort.j]
+  out$impute_sequence <- impute_sequence
   out$iter <- iter
   out$maxiter <- maxiter
 
